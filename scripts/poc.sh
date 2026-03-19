5042890038b1b22c5d6839e40d7fe5f3e56a9842
Soumya Shaw <soshaw@lxportal1.physik.rwth-aachen.de>
2026-03-19 15:54:05 +0100

    deleted old files

--- scripts/poc.sh
+++ scripts/poc.sh
@@ -13,7 +13,7 @@ cd $HOME/AnomalyDetection
 apptainer exec --nv $IMG bash -c "
     source /opt/conda/bin/activate
     cd \"$HOME/AnomalyDetection\"
-    python -m src.poc_expts.expt_batching_aachen --gpu_id 0 --jet_name both --merge_strategy concat --naming_identifier poc_baseline_aachen --guaranteed_signal_per_batch 0 --use_wandb
+    python -m src.poc_expts.expt_batching_aachen --gpu_id 0 --jet_name both --merge_strategy concat --naming_identifier poc_batch2_aachen --guaranteed_signal_per_batch 2 --use_wandb
     "
 
 echo "Job finished."

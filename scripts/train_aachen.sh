bee20a199cb41818e56aa6bd41b9193490480368
Soumya Shaw <soshaw@lxportal1.physik.rwth-aachen.de>
2026-04-02 19:35:43 +0200

    update

--- scripts/train_aachen.sh
+++ scripts/train_aachen.sh
@@ -15,7 +15,7 @@ apptainer exec --nv "$IMG" bash -lc '
     source /opt/conda/bin/activate
     cd "$HOME/AnomalyDetection"
 
-    python -m src.train.train_custom_aachen --gpu_id 0 --seed 42 --jet_name both --merge_strategy concat --naming_identifier weak_600_pretrain_gen1M_update_aachen --pretrained_ckpt backbone_weights/pretrained_gen_1M_each/backbone.ckpt --load_pretrained --update_backbone --use_hpc --use_wandb
+    python -m src.train.train_custom_aachen --gpu_id 0 --seed 42 --jet_name both --merge_strategy concat --naming_identifier weak_10k_pretrain_gen1M_frozen_aachen --pretrained_ckpt backbone_weights/pretrained_gen_1M_each/backbone.ckpt --load_pretrained --freeze_backbone --use_hpc --use_wandb
 '
 
 echo "Job finished."

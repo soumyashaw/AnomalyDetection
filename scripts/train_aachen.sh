8374424ce214c70f5e6f09fcb24643c4553bcc1f
rww74320 <rww74320@login23-1.hpc.itc.rwth-aachen.de>
2026-04-19 16:50:27 +0200

    Resolve merge conflict in scripts/train_aachen.sh: accept theirs

--- scripts/train_aachen.sh
+++ scripts/train_aachen.sh
@@ -15,9 +15,7 @@ apptainer exec --nv "$IMG" bash -lc '
     source /opt/conda/bin/activate
     cd "$HOME/AnomalyDetection"
 
-    python -m src.train.train_custom_aachen --seed 42 --jet_name both --merge_strategy concat --naming_identifier weak_600_optimal_wopretrain_aachen --use_hpc --use_wandb
+    python -m src.train.train_custom_aachen --seed 42 --jet_name both --merge_strategy concat --naming_identifier weak_10k_pretrain_genclass10M_freeze_aachen --pretrained_ckpt backbone_weights/pretrained_gen_class_10M/backbone.ckpt --load_pretrained --freeze_backbone --use_hpc --use_wandb
 '
 
 echo "Job finished."
-
-# python -m src.train.train_custom_aachen --seed 42 --jet_name both --merge_strategy concat --naming_identifier weak_10k_pretrain_genclass10M_freeze_aachen --pretrained_ckpt backbone_weights/pretrained_gen_class_10M/backbone.ckpt --load_pretrained --freeze_backbone --use_hpc --use_wandb
